122affe60dda505ee7913a12b912b0fece79c623
Alan Osvaldo Mayorga <66398974+AlanMayorga@users.noreply.github.com>
2023-10-06 23:06:45 -0600

    Update astro.config.mjs
    Se eliminó configuración de github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://alanmayorga.github.io',
-  base: '/'
+  integrations: [tailwind()]
 });
